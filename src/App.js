109fe34976a7a3845fa4ef2b2df0a8572cc1e3d3
OksanaYur <107573946+OksanaYur@users.noreply.github.com>
2022-10-06 09:09:04 +0300

    added github link

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-    </div>
-  );
+      <h>Weather App</h>
+      <footer>
+        This project was coded by Oksana Yuriychuk and is {""}
+      <a href='https://github.com/OksanaYur/week-5-react' target="_blank">
+        open-sourced on GitHub</a> 
+        </footer>   
+        </div>
+   );
 }
 
 
