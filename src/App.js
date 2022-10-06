cd2f8416c467373e64bcef28bdc7bb18a2c289db
OksanaYur <107573946+OksanaYur@users.noreply.github.com>
2022-10-06 09:32:14 +0300

    boootstrap was added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h>Weather App</h>
+      <div className='container'>
+      <h1>Weather App</h1>
       <footer>
-        This project was coded by Oksana Yuriychuk and is {""}
+        This project was coded by Oksana Yuriychuk and is {" "}
       <a href='https://github.com/OksanaYur/week-5-react' target="_blank">
         open-sourced on GitHub</a> 
-        </footer>   
+        </footer>  
+        </div> 
         </div>
    );
 }
